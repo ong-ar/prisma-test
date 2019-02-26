c7db66f8c544ea87bdbef72e35f14dc8c01f0782
ong-ar <reclusis@gmail.com>
2019-02-26 16:04:56 +0900

    값 가져오기 테스트

--- index.js
+++ index.js
@@ -2,29 +2,15 @@ const { prisma } = require("./generated/prisma-client");
 
 // A `main` function so that we can use async/await
 async function main() {
-  // Create a new user with a new post
-  const newUser = await prisma.createUser({
-    name: "Bob",
-    email: "bob@prisma.io",
-    posts: {
-      create: [
-        {
-          title: "Join us for GraphQL Conf in 2019"
-        },
-        {
-          title: "Subscribe to GraphQL Weekly for GraphQL news"
-        }
-      ]
-    }
-  });
-  console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`);
-
-  // Read all users from the database and print them to the console
-  const allUsers = await prisma.users();
-  console.log(allUsers);
-
-  const allPosts = await prisma.posts();
-  console.log(allPosts);
+  // Read the previously created user from the database and print their posts to the console
+  const postsByUser = await prisma.user({ email: "bob@prisma.io" }).posts();
+  const userByPost = await prisma
+    .post({ id: "cjslf4v1b00cp0831hbkb1jk6" })
+    .author();
+  const i = await prisma.users().posts();
+  console.log(`All posts by that user: ${JSON.stringify(postsByUser)}`);
+  console.log(`user by post: ${JSON.stringify(userByPost)}`);
+  console.log(`all posts by all users: ${JSON.stringify(i)}`);
 }
 
 main().catch(e => console.error(e));
